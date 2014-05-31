2f34fe017c971e7d85d01e4fce06541d92a46a79
Daniel Francis <francis@sugarlabs.org>
2014-05-31 13:27:55 -0300

    Add models
    Signed-off-by: Daniel Francis <francis@sugarlabs.org>
    Signed-off-by: Pablo Piriz <pablopiriz1313@gmail.com>
    Signed-off-by: Federico Palacios <fedenator7@gmail.com>

--- src/esicalendar/admin.py
+++ src/esicalendar/admin.py
@@ -2,9 +2,13 @@ from django.contrib import admin
 from esicalendar.models import Place
 from esicalendar.models import PlaceType
 from esicalendar.models import Subject
+from esicalendar.models import SubjectGroup
 from esicalendar.models import User
+from esicalendar.models import Event
 
 admin.site.register(Place)
 admin.site.register(PlaceType)
 admin.site.register(Subject)
+admin.site.register(SubjectGroup)
 admin.site.register(User)
+admin.site.register(Event)
--- src/esicalendar/models.py
+++ src/esicalendar/models.py
@@ -1,5 +1,8 @@
+# -*- coding: utf-8 -*-
+
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import Group
 
 
 class PlaceType(models.Model):
@@ -26,7 +29,36 @@ class Subject(models.Model):
 
 class User(AbstractUser):
     idnumber = models.IntegerField(null=True, blank=True, default=0)
-#    approved = models.BooleanField(default=False)
+    approved = models.BooleanField(default=False)
 
     def __unicode__(self):
         return self.get_full_name()
+
+
+class SubjectGroup(models.Model):
+    subject = models.ForeignKey(Subject)
+    group = models.ForeignKey(Group)
+    teacher = models.ForeignKey(User)
+    hours = models.IntegerField(default=1)
+
+    def __unicode__(self):
+        return '%s %s' % (self.subject, self.group)
+
+
+class Event(models.Model):
+    subjectgroup = models.ForeignKey(SubjectGroup)
+    place = models.ForeignKey(Place)
+    start = models.TimeField()
+    hours = models.IntegerField(default=1)
+    date = models.CharField(max_length=3,
+                            choices=(
+                                ('SAT', 'Sábado'),
+                                ('MON', 'Lunes'),
+                                ('TUE', 'Martes'),
+                                ('WED', 'Miércoles'),
+                                ('THU', 'Jueves'),
+                                ('FRI', 'Viernes')
+                            ))
+
+    def __unicode__(self):
+        return '%s %s %s' % (self.subjectgroup, self.date, self.start)
