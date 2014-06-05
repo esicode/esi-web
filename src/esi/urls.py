7ad0cfca668ca31082641b0b6fd17275d843103f
Daniel Francis <francis@sugarlabs.org>
2014-06-05 17:57:46 -0300

    Add api views for Users and groups
    Signed-off-by: Daniel Francis <francis@sugarlabs.org>

--- src/esi/urls.py
+++ src/esi/urls.py
@@ -3,10 +3,15 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'esi.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+from django.views.decorators.csrf import csrf_exempt
 
+from esicalendar.views import GroupListAPIView
+from esicalendar.views import GroupAPIView
+from esicalendar.views import UserAPIView
+
+urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^api/groups/(?P<pk>[0-9])', csrf_exempt(GroupAPIView.as_view())),
+    url(r'^api/groups', csrf_exempt(GroupListAPIView.as_view())),
+    url(r'^api/users/(?P<pk>[0-9])', csrf_exempt(UserAPIView.as_view())),
 )
